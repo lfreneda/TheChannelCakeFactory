48ecf05d7f2493711d2a50de7fe2d826d1ccb1fd
Luiz Freneda <lfreneda@gmail.com>
2015-05-29 14:28:44 -0300

    nuspec

--- src/TheChannelCakeFactory/Properties/AssemblyInfo.cs
+++ src/TheChannelCakeFactory/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TheChannelCakeFactory")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Its a little/tiny/simple way to create your proxy/channel from a service contract")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TheChannelCakeFactory")]
